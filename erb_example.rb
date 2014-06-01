60bf7456b66f40a46c2f0a65ee99dab37c1a7035
Kody Wilson <kodywilson@gmail.com>
2014-06-01 06:17:19 -0700

    Completed first exercise

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,9 @@
 require 'erb'
 
 x = 42
+y = 24
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+test = ERB.new "<%= x %> + <%= y %> is equat to <%= x + y %>"
+puts
+puts test.result(binding)
